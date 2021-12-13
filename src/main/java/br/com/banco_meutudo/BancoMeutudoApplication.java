fb5533022fe2bf9cfb21973a091a10b9ae5eff26
Romulo Santos <romulosantos.ti@gmail.com>
2021-12-13 20:38:44 -0300

    Melhorando gitignore. Adicionando arquivos iniciais do projeto.

---
+++ src/main/java/br/com/banco_meutudo/BancoMeutudoApplication.java
@@ -0,0 +1,15 @@
+package br.com.banco_meutudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoMeutudoApplication {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(BancoMeutudoApplication.class, args);
+
+    }
+
+}
